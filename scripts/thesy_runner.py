617a60c655f9e7b4e9f31017021fd493d0d8ef55
Cole K <cole-k@users.noreply.github.com>
2024-07-06 23:02:45 -0700

    more bugs in thesy runner

--- scripts/thesy_runner.py
+++ scripts/thesy_runner.py
@@ -51,6 +51,7 @@ def _run_thesy(task_name, inp_file, output_path, extra_flag):
 
 def _collect_result(output_dir, summary_path, dataset):
     outfile = os.path.join(summary_path, dataset + '.csv')
+    util.create_path(outfile)
     stats_processor.write_stats(output_dir, outfile)
 
 def get_runner():
